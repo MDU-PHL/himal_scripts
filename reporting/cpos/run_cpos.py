664522582c80323d8eb044662e8b6155313315e0
Himal Shrestha <himal.shrestha@unimelb.edu.au>
2024-09-11 11:51:57 +1000

    modified cpos.py to accept multiple isolates of interest as input

--- reporting/cpos/run_cpos.py
+++ reporting/cpos/run_cpos.py
@@ -2,16 +2,21 @@ import typer
 
 def generate_run_me(
     job_id: str = typer.Argument(..., help="Job ID (e.g., XX_20xxxxxx_ecoli_STxxx)"),
-    isolate_of_interest: str = typer.Argument(..., help="Isolate of Interest (e.g., 2024-interest)"),
+    isolates_of_interest: str = typer.Argument(..., help="Isolates of Interest (comma-separated, e.g., 2024-interest1,2024-interest2)"),
     reference: str = typer.Option("reference", "--reference", "-r", help="Reference (optional)"),
     st: int = typer.Option(00, "--st", "-s", help="Sequence Type (optional, default: 00)"),
     user: str = typer.Option("user", "--user", "-u", help="User (optional, default: user)"),
     species: str = typer.Option("genus species", "--species", "-sp", help="Species (optional, default: genus species)")
 ):
+    isolates = isolates_of_interest.split(',')
     content = f"""
 mkdir /home/mdu/analysis/sharepoint/2024/{job_id} && cd /home/mdu/analysis/sharepoint/2024/{job_id}/
-
-mdu search -id {isolate_of_interest} --csv | csvtk transpose | csvtk pretty
+"""
+    for isolate in isolates:
+        content += f"""
+mdu search -id {isolate} --csv | csvtk transpose | csvtk pretty
+"""
+    content += f"""
 ls .. | grep -i st{st}
 ls -lhart .. | grep -i st{st}
 ls ../../../meistertask/*/ | grep -i st{st}
@@ -22,8 +27,12 @@ mdu search -sp "{species}" -st {st} > search.tab
 csvtk split -t -f "QC" search.tab
 csvtk cut -t -f "ISOLATE" search-PASS.tab > list.txt
 sed -i '1d' list.txt ## remove the first/header line
-grep {isolate_of_interest} list.txt
-
+"""
+    for isolate in isolates:
+        content += f"""
+grep {isolate} list.txt
+"""
+    content += f"""
 mdu reads --idfile list.txt > reads.tab
 mdu contigs --idfile list.txt --assemblier shovill > contigs.tab
 wc -l reads.tab contigs.tab list.txt
@@ -39,28 +48,37 @@ mkdir -p /home/{user}/public_html/MDU/REPORTS/CPO/{job_id}/
 cp /home/mdu/analysis/sharepoint/2024/{job_id}/report/report_default.html /home/{user}/public_html/MDU/REPORTS/CPO/{job_id}/
 
 # Go to the link below to see the report: https://bioinformatics.mdu.unimelb.edu.au/~{user}/MDU/REPORTS/CPO/{job_id}/report_default.html
-
-csvtk tab2csv report/distances.tab | csvtk cut -F -f Isolate,"*{isolate_of_interest}" | csvtk sort -k 2:n | head
-
+"""
+    for isolate in isolates:
+        content += f"""
+csvtk tab2csv report/distances.tab | csvtk cut -F -f Isolate,"*{isolate}" | csvtk sort -k 2:n | head
+"""
+    content += f"""
 # Runing bohra-pretty
 mkdir bohra-pretty && cd bohra-pretty
 python /home/himals/3_resources/github-repos/himal_scripts/reporting/cpos/update_cpo_report.py -u "Himal Shrestha" -j "{job_id}" 
 
 echo "Isolate" > ../report/Focus_isolates.txt
-echo "{isolate_of_interest}" >> ../report/Focus_isolates.txt
-
+"""
+    for isolate in isolates:
+        content += f"""
+echo "{isolate}" >> ../report/Focus_isolates.txt
+"""
+    content += f"""
 ca my_renv
 Rscript /home/himals/3_resources/tools/scripts/bohra-pretty/render_report.R ../report/
 cp cpo_report.html /home/{user}/public_html/MDU/REPORTS/CPO/{job_id}/
 
 # Go to the link below to see the report: https://bioinformatics.mdu.unimelb.edu.au/~{user}/MDU/REPORTS/CPO/{job_id}/cpo_report.html
-
-
-# finding unique plasmids, virulence, and resistance factors
-python /home/himals/3_resources/github-repos/himal_scripts/bohra/isolate_plasmids.py ../report/plasmid.txt {isolate_of_interest}
-python /home/himals/3_resources/github-repos/himal_scripts/bohra/process_resistome_virulome.py ../report/resistome.txt {isolate_of_interest}
-python /home/himals/3_resources/github-repos/himal_scripts/bohra/process_resistome_virulome.py ../report/virulome.txt {isolate_of_interest}
-
+"""
+    for isolate in isolates:
+        content += f"""
+python /home/himals/3_resources/github-repos/himal_scripts/bohra/isolate_plasmids.py ../report/plasmid.txt {isolate}
+python /home/himals/3_resources/github-repos/himal_scripts/bohra/process_resistome_virulome.py ../report/resistome.txt {isolate}
+python /home/himals/3_resources/github-repos/himal_scripts/bohra/process_resistome_virulome.py ../report/virulome.txt {isolate}
+python /home/himals/3_resources/github-repos/himal_scripts/bohra/process_resistome_virulome.py ../report/typer.txt {isolate}
+"""
+    content += f"""
 # ---
 Job ID
 {job_id}
@@ -71,7 +89,7 @@ https://bioinformatics.mdu.unimelb.edu.au/~{user}/MDU/REPORTS/CPO/{job_id}/cpo_r
 Reference
 {reference}-shovill-ref.fa
 Isolate of Interest
-{isolate_of_interest}
+{isolates_of_interest}
 SNPs
 <Fill up SNP details>
 Resistome: Nothing of Note
