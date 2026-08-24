a88d40158961aceda35583049dc363925d90590f
Himal Shrestha <himal.shrestha@unimelb.edu.au>
2026-08-24 12:33:30 +1000

    feat(run_cpos): update run script for screening research isolates changing read order

--- reporting/cpos/run_cpos.py
+++ reporting/cpos/run_cpos.py
@@ -10,15 +10,15 @@ def generate_run_me(
 ):
     isolates = isolates_of_interest.split(',')
     content = f"""
-mkdir /home/mdu/analysis/sharepoint/2024/{job_id} && cd /home/mdu/analysis/sharepoint/2024/{job_id}/
+mkdir /home/mdu/analysis/sharepoint/2026/{job_id} && cd /home/mdu/analysis/sharepoint/2026/{job_id}/
 """
     for isolate in isolates:
         content += f"""
-mdu search -id {isolate} --csv | csvtk transpose | csvtk pretty
+mdu search -id {isolate} --csv | csvtk transpose | csvtk pretty | less -S
 """
     content += f"""
-ls .. | grep -i st{st}
-ls -lhart .. | grep -i st{st}
+ls ../../*/ | grep -i st{st}
+ls -lhart ../../*/ | grep -i st{st}
 ls ../../../meistertask/*/ | grep -i st{st}
 ls -lhart ../../../meistertask/*/ | grep -i st{st}
 
@@ -33,9 +33,18 @@ sed -i '1d' list.txt ## remove the first/header line
 grep {isolate} list.txt
 """
     content += f"""
-mdu reads --idfile list.txt > reads.tab
+
+# screen for any isolates that should not be analysed
+grep -f list.txt /home/mdu/analysis/surveillance/isolates_lists/*txt | less
+
+mdu reads --idfile list.txt > reads_raw.tab
 mdu contigs --idfile list.txt --assemblier shovill > contigs.tab
+
+# change 1.trim to 2.trim in column 2, and 2.trim to 1.trim in column 3
+awk 'BEGIN{{OFS=FS="\\t"}} {{gsub("2\\\\.trim","1.trim",$2); gsub("1\\\\.trim","2.trim",$3); print}}' reads_raw.tab > reads.tab
+
 wc -l reads.tab contigs.tab list.txt
+
 grep {reference} contigs.tab
 
 cp <path_from_above_grep_command> {reference}-shovill-ref.fa
@@ -45,7 +54,7 @@ conda activate bohra_nf
 bohra run -i reads.tab -c contigs.tab -r {reference}-shovill-ref.fa -j {job_id} -p default --cpus 150 --proceed
 
 mkdir -p /home/{user}/public_html/MDU/REPORTS/CPO/{job_id}/
-cp /home/mdu/analysis/sharepoint/2024/{job_id}/report/report_default.html /home/{user}/public_html/MDU/REPORTS/CPO/{job_id}/
+cp /home/mdu/analysis/sharepoint/2026/{job_id}/report/report_default.html /home/{user}/public_html/MDU/REPORTS/CPO/{job_id}/
 
 # Go to the link below to see the report: https://bioinformatics.mdu.unimelb.edu.au/~{user}/MDU/REPORTS/CPO/{job_id}/report_default.html
 """
@@ -66,7 +75,7 @@ echo "{isolate}" >> ../report/Focus_isolates.txt
 """
     content += f"""
 ca my_renv
-Rscript /home/himals/3_resources/tools/scripts/bohra-pretty/render_report.R ../report/
+Rscript /home/himals/3_resources/tools/scripts/bohra-pretty/render_report.R ../report/ {job_id}
 cp cpo_report.html /home/{user}/public_html/MDU/REPORTS/CPO/{job_id}/
 
 # Go to the link below to see the report: https://bioinformatics.mdu.unimelb.edu.au/~{user}/MDU/REPORTS/CPO/{job_id}/cpo_report.html
@@ -75,8 +84,6 @@ cp cpo_report.html /home/{user}/public_html/MDU/REPORTS/CPO/{job_id}/
         content += f"""
 python /home/himals/3_resources/github-repos/himal_scripts/bohra/isolate_plasmids.py ../report/plasmid.txt {isolate}
 python /home/himals/3_resources/github-repos/himal_scripts/bohra/process_resistome_virulome.py ../report/resistome.txt {isolate}
-python /home/himals/3_resources/github-repos/himal_scripts/bohra/process_resistome_virulome.py ../report/virulome.txt {isolate}
-python /home/himals/3_resources/github-repos/himal_scripts/bohra/process_resistome_virulome.py ../report/typer.txt {isolate}
 """
     content += f"""
 # ---
@@ -92,9 +99,9 @@ Isolate of Interest
 {isolates_of_interest}
 SNPs
 <Fill up SNP details>
-Resistome: Nothing of Note
-Virulome: Nothing of Note
-Plasmids: Nothing of Note
+Plasmid Summary:
+
+@Catherine please find the analysis in the Reports section and attached Word doc. Thank you 🙏😊.
 """
     
     filename = f"run_me_{job_id}.txt"
